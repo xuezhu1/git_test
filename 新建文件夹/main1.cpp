948713c79af4425478283106c3bd976927f4ff45
“xuezhu <xuezhu2@yeah.net>
2019-10-13 12:42:39 +0800

    init

---
+++ 新建文件夹/main1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+int main(){
+
+
+    int a=2;
+    int b=4;
+    while(std::cin >>a){
+        std::cout <<"sum is"<<a;
+    }
+    return 0;
+}
+#111
+<<<<<<< HEAD
+dev win 11 :24
+=======
+
+ubuntu dev 11:37
+
+
+
+>>>>>>> 9f9a88823a3de1a54d43b02773fc23a87a2d2244
